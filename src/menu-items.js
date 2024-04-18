e5769307a6d7f839d9e12bc9b0c9fdb9514a69e0
DLKReact@2024 <dlktechnologiesreact@gmail.com>
2024-04-18 09:45:34 +0530

    first commit

---
+++ src/menu-items.js
@@ -0,0 +1,83 @@
+const menuItems = {
+  items: [
+    {
+      id: 'navigation',
+      title: 'Navigation',
+      type: 'group',
+      icon: 'icon-navigation',
+      children: [
+        {
+          id: 'dashboard',
+          title: 'Dashboard',
+          type: 'item',
+          icon: 'feather icon-home',
+          url: '/app/dashboard/default'
+        }
+      ]
+    },
+    
+    {
+      id: 'ui-forms',
+      title: 'FORMS & TABLES',
+      type: 'group',
+      icon: 'icon-group',
+      children: [
+        {
+          id: 'forms',
+          title: 'Form Elements',
+          type: 'item',
+          icon: 'feather icon-file-text',
+          url: '/forms/form-basic'
+        },
+        {
+          id: 'table',
+          title: 'Table',
+          type: 'item',
+          icon: 'feather icon-server',
+          url: '/tables/bootstrap'
+        }
+      ]
+    },
+    
+    {
+      id: 'pages',
+      title: 'Pages',
+      type: 'group',
+      icon: 'icon-pages',
+      children: [
+        {
+          id: 'auth',
+          title: 'Authentication',
+          type: 'collapse',
+          icon: 'feather icon-lock',
+          badge: {
+            title: 'New',
+            type: 'label-danger'
+          },
+          children: [
+            {
+              id: 'signup-1',
+              title: 'Sign up',
+              type: 'item',
+              url: '/auth/signup-1',
+              target: true,
+              breadcrumbs: false
+            },
+            {
+              id: 'signin-1',
+              title: 'Sign in',
+              type: 'item',
+              url: '/auth/signin-1',
+              target: true,
+              breadcrumbs: false
+            }
+          ]
+        },
+        
+        
+      ]
+    }
+  ]
+};
+
+export default menuItems;
