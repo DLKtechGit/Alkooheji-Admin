881ebca190d0b3a327f3caf3763230ebbea4cf0d
DLKReact@2024 <dlktechnologiesreact@gmail.com>
2024-04-18 10:48:58 +0530

    title changed

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
 export const BASE_URL = '/auth/signin-1';
-export const BASE_TITLE = ' | Datta Able Premium React Hooks + Redux Admin Template';
+export const BASE_TITLE = ' | Alkooheji';
 
 export const CONFIG = {
   layout: 'vertical',
